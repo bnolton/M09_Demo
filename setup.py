ff500b4f5c248ff31dc00a7efba530d08a7d1e22
bnolton <brian.nolton@outlook.com>
2024-10-29 20:18:53 -0400

    m09

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/bnolton/M09_Demo',
+    author='Brian Nolton',
+    author_email='frv3fp@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
